b6818f773f1055a3795b775ff65c98ac92694a89
Patryk Kieszek <pkieszek@c2r3s4.42warsaw.pl>
2025-01-31 16:52:40 +0100

    handle hex

--- handle_hex.c
+++ handle_hex.c
@@ -1,17 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_hex_lower.c                                 :+:      :+:    :+:   */
+/*   handle_hex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:21:16 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/01/31 12:19:17 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/01/31 16:47:20 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
-	%x → liczba w systemie szesnastkowym (małe litery)
+	%x/%X → liczba w systemie szesnastkowym (małe litery)/(duze litery)
 */
 
 #include "ft_printf.h"
+
+int ft_print_hex(unsigned int num, int uppercase)
+{
+    char str[8];
+    int i = 0;
+    char *hex = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+
+    if (num == 0)
+        return (write(1, "0", 1));
+
+    while (num)
+    {
+        str[i++] = hex[num % 16];
+        num /= 16;
+    }
+
+    int len = i;
+    while (i--)
+        write(1, &str[i], 1);
+
+    return (len);
+}
