8fcdca562b748222a9fe2da785067761a1787bdc
Patryk Kieszek <pkieszek@c2r3s4.42warsaw.pl>
2025-01-31 16:15:02 +0100

    handle string done

--- handle_string.c
+++ handle_string.c
@@ -6,7 +6,7 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:08:49 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/01/31 12:16:40 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/01/31 16:14:39 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,17 @@
 */
 
 #include "ft_printf.h"
+
+int ft_print_string(char *str)
+{
+    int len;
+
+    if (!str)
+        str = "(null)";
+
+    len = ft_strlen(str);
+	
+    ft_putstr_fd(str, 1);
+
+    return (len);
+} 
