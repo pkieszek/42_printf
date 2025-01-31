57bb531dd5336dfa4a4d51a5077d6702a411ba90
Patryk Kieszek <pkieszek@c2r3s4.42warsaw.pl>
2025-01-31 12:53:37 +0100

    structure of printf

--- ft_printf.h
+++ ft_printf.h
@@ -1,7 +1,3 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-va_list args;
-va_start( args, format);
-va_arg( va_list var, type_of_the_variable );
-va_end(args);
---
+++ ft_pritnf.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pritnf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 12:39:51 by pkieszek          #+#    #+#             */
+/*   Updated: 2025/01/31 12:52:08 by pkieszek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+
+}
+va_list args;
+va_start( args, format);
+va_arg( va_list var, type_of_the_variable );
+va_end(args);
