f2dc5cd1c290705899a83a76198c1e5aa32bbc86
Patryk Kieszek <pkieszek@c2r3s4.42warsaw.pl>
2025-01-31 16:22:36 +0100

    handle signed integers

--- handle_int.c
+++ handle_int.c
@@ -6,12 +6,28 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:25:24 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/01/31 12:26:13 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/01/31 16:22:17 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
-	%i → liczba całkowita (base 10)
+	%i/%d → liczba całkowita (base 10), signed integers
 */
 
 #include "ft_printf.h"
+
+int ft_print_int(int n)
+{
+    char *num_str;
+    int len;
+
+    num_str = ft_itoa(n); // Convert integer to string
+    if (!num_str)
+        return (0);
+
+    ft_putstr_fd(num_str, 1); // Print the string
+    len = ft_strlen(num_str); // Get the printed length
+    free(num_str); // Free the allocated memory
+
+    return (len);
+}
