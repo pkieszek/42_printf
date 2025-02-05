3850c51c48d448f8094967ac8b59c07e6402ecc9
Patryk Kieszek <pkieszek@c2r3s2.42warsaw.pl>
2025-02-05 16:59:30 +0100

    printf changing format, name of pointer

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_16_p.c                                      :+:      :+:    :+:   */
+/*   ft_print_pointer.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:29:40 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/01/31 12:30:11 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/05 16:41:38 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,23 @@
 */
 
 #include "ft_printf.h"
+
+static void ft_puthex(unsigned long num, int *count)
+{
+    char *hex = "0123456789abcdef";
+
+    if (num >= 16)
+        ft_puthex(num / 16, count);
+    *count += ft_putchar_fd(hex[num % 16], 1);
+}
+
+void ft_print_pointer(void *ptr, int *count)
+{
+    if (!ptr)
+    {
+        *count += ft_putstr_fd("(nil)", 1);
+        return;
+    }
+    *count += ft_putstr_fd("0x", 1);
+    ft_puthex((unsigned long)ptr, count);
+}
--- ft_pritnf.c
+++ ft_pritnf.c
@@ -6,20 +6,31 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 12:39:51 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/01/31 15:56:14 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/05 16:58:41 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-int check()
-{
-    
-}
 
-int	ft_printf(const char *format, ...)
+void	ft_format(va_list va, char *str, size_t *counter)
 {
-va_list args;
-va_start( args, format);
-va_arg( va_list var, type_of_the_variable );
-va_end(args);
+	if (*str == 'c')
+		ft_putchar_pf(va_arg(va, int), counter);
+	else if (*str == 's')
+		ft_putstr_pf(va_arg(va, char *), counter);
+	else if (*str == 'p')
+		ft_putptr_pf(va_arg(va, void *), counter);
+	else if (*str == 'i' || *str == 'd')
+		ft_putnbr_pf(va_arg(va, int), counter);
+	else if (*str == 'u')
+		ft_putuint_pf(va_arg(va, unsigned int), counter);
+	else if (*str == 'x' || *str == 'X')
+	{
+		if (*str == 'x')
+			ft_puthex_pf(va_arg(va, unsigned int), counter, HEX_LOW_BASE);
+		else
+			ft_puthex_pf(va_arg(va, unsigned int), counter, HEX_UPP_BASE);
+	}
+	else if (*str == '%')
+		ft_putchar_pf(*str, counter);
 }
