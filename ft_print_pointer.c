ecbd405bd0c126be7146064c91286989aa4b84f3
Patryk <fierte.06.signaler@icloud.com>
2025-02-18 17:50:47 +0100

    corrected

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:24:42 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/02/18 17:24:55 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/18 17:49:37 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,24 @@
 /* Helper function to print a number in hexadecimal format */
 static void ft_puthex(unsigned long num)
 {
-    char *hex;
+    char *hex = "0123456789abcdef";
 
-    hex = "0123456789abcdef";
     if (num >= 16)
         ft_puthex(num / 16);
     ft_putchar_fd(hex[num % 16], 1);
+}
+
+/* Helper function to calculate the length of a hexadecimal number */
+static int ft_hexlen(unsigned long num)
+{
+    int len = 1;
+
+    while (num >= 16)
+    {
+        num /= 16;
+        len++;
+    }
+    return (len);
 }
 
 /* Function to print a pointer address and return its length */
@@ -29,9 +41,12 @@ int ft_print_pointer(void *ptr)
 {
     int len;
 
-    if (!ptr) // Handle NULL pointer case
+    if (!ptr)
         return (ft_putstr_fd("(nil)", 1), 5);
-    len = ft_putstr_fd("0x", 1); // Print the '0x' prefix
+    
+    ft_putstr_fd("0x", 1);
+    len = 2; // '0x' prefix is always 2 characters long
     ft_puthex((unsigned long)ptr); // Print the memory address in hex
-    return (len + ft_strlen(ft_itoa_base((unsigned long)ptr, 16))); // Return total length
+
+    return (len + ft_hexlen((unsigned long)ptr)); // Return total length
 }
