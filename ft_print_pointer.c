836e6d6789be240250eac41886c7ffd0e715400c
Patryk <fierte.06.signaler@icloud.com>
2025-02-20 01:01:46 +0100

    final version, norminette ok

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:24:42 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/02/19 02:15:51 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/20 01:01:20 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,21 @@
 #include "libft.h"
 
 /*
- * Helper function to print a number in hexadecimal format.
+ * Function: ft_puthex
+ * -------------------
+ * Recursively prints a number in lowercase hexadecimal format.  
+ *
+ * Parameters:
+ * - num: The unsigned long number to be printed in hexadecimal.  
+ *
+ * How it works:
+ * 1. The hexadecimal digits are stored in a string `"0123456789abcdef"`.  
+ * 2. If the number is 16 or greater, the function calls itself recursively  
+ *    to print the higher-order digits first.  
+ * 3. The least significant digit is printed using `ft_putchar_fd()`.  
+ *
+ * Example:
+ * Calling `ft_puthex(42)` prints `"2a"`.  
  */
 static void	ft_puthex(unsigned long num)
 {
@@ -27,7 +41,24 @@ static void	ft_puthex(unsigned long num)
 }
 
 /*
- * Helper function to calculate the length of a hexadecimal number.
+ * Function: ft_hexlen
+ * -------------------
+ * Computes the length of a number when represented in hexadecimal.  
+ *
+ * Parameters:
+ * - num: The unsigned long number to measure.  
+ *
+ * How it works:
+ * 1. The function starts with `len = 1`, since every number has at  
+ *    least one digit.  
+ * 2. It repeatedly divides the number by 16, increasing `len` each  
+ *    time, until the number is smaller than 16.  
+ *
+ * Return:
+ * - The number of characters required to represent `num` in hexadecimal.  
+ *
+ * Example:
+ * Calling `ft_hexlen(42)` returns `2` because `"2a"` is two characters long.  
  */
 static int	ft_hexlen(unsigned long num)
 {
@@ -43,7 +74,26 @@ static int	ft_hexlen(unsigned long num)
 }
 
 /*
- * Function to print a pointer in hexadecimal format.
+ * Function: ft_print_pointer
+ * --------------------------
+ * Prints a memory address in hexadecimal format with the `0x` prefix.  
+ *
+ * Parameters:
+ * - ptr: A pointer to be printed.  
+ *
+ * How it works:
+ * 1. If `ptr` is `NULL`, it prints `"(nil)"` and returns `5` (the length).  
+ * 2. Otherwise, it prints `"0x"` to indicate a hexadecimal memory address.  
+ * 3. It calls `ft_puthex()` to print the pointer's address as a hexadecimal  
+ *    number.  
+ * 4. The function calculates the total length (2 for `"0x"` + the hex digits)  
+ *    and returns it.  
+ *
+ * Return:
+ * - The total number of characters printed.  
+ *
+ * Example:
+ * Calling `ft_print_pointer((void *)42)` prints `"0x2a"` and returns `4`.  
  */
 int	ft_print_pointer(void *ptr)
 {
