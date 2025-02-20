258f017319d067842167ce6180cbfbb513c302f1
Patryk <fierte.06.signaler@icloud.com>
2025-02-20 01:03:35 +0100

    final version, norminette ok

--- ft_print_int.c
+++ ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:00:44 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/02/19 02:11:16 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/20 01:03:11 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,28 @@
 #include "libft.h"
 
 /*
- * Converts an integer to a string, prints it, and returns its length.
+ * Function: ft_print_int
+ * ----------------------
+ * Converts an integer to a string, prints it, and returns its length.  
+ *
+ * Parameters:
+ * - num: The integer to be printed.  
+ *
+ * How it works:
+ * 1. Calls `ft_itoa()` to convert the integer into a dynamically allocated  
+ *    string representation.  
+ * 2. If `ft_itoa()` fails and returns `NULL`, the function immediately  
+ *    returns `0` to indicate failure.  
+ * 3. Calls `ft_strlen()` to determine the length of the string.  
+ * 4. Prints the string using `ft_putstr_fd()`.  
+ * 5. Frees the dynamically allocated memory to avoid memory leaks.  
+ * 6. Returns the number of characters printed.  
+ *
+ * Return:
+ * - The number of characters printed.  
+ *
+ * Example:
+ * Calling `ft_print_int(42);` prints `"42"` and returns `2`.  
  */
 int	ft_print_int(int num)
 {
