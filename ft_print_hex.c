4e470facd9e488bf6d739c09c93681f1ff45a9f3
Patryk <fierte.06.signaler@icloud.com>
2025-02-20 01:06:23 +0100

    final version, norminette ok

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -6,7 +6,7 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:26:35 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/02/20 00:49:42 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/20 01:05:56 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 #include "libft.h"
 
 /*
- * Helper function to print a number in hexadecimal format.
+ * Function: ft_puthex
+ * -------------------
+ * Recursively prints an unsigned integer in hexadecimal format.  
+ *
+ * Parameters:
+ * - num: The unsigned integer to print.  
+ * - base: A string containing hexadecimal digits (either lowercase or  
+ *   uppercase).  
+ *
+ * How it works:
+ * 1. If the number is greater than or equal to 16, the function calls  
+ *    itself recursively with `num / 16`.  
+ * 2. This ensures that the most significant digit is printed first.  
+ * 3. After recursion, the current digit is printed using `ft_putchar_fd()`.  
+ * 4. The `base` string determines whether the output is lowercase (`"x"`)  
+ *    or uppercase (`"X"`).  
+ *
+ * Example:
+ * Calling `ft_puthex(42, "0123456789abcdef")` prints `"2a"`.  
  */
 static void	ft_puthex(unsigned int num, char *base)
 {
@@ -24,7 +42,21 @@ static void	ft_puthex(unsigned int num, char *base)
 }
 
 /*
- * Helper function to calculate the length of a hexadecimal number.
+ * Function: ft_hexlen
+ * -------------------
+ * Computes the number of characters needed to represent a number  
+ * in hexadecimal format.  
+ *
+ * Parameters:
+ * - num: The unsigned integer whose hexadecimal length is calculated.  
+ *
+ * How it works:
+ * 1. Initializes `len` to `1` because every number has at least one digit.  
+ * 2. Keeps dividing `num` by `16` and increasing `len` until `num < 16`.  
+ * 3. Returns the total length of the hexadecimal representation.  
+ *
+ * Example:
+ * Calling `ft_hexlen(42)` returns `2` because `"2a"` is two characters long.  
  */
 static int	ft_hexlen(unsigned int num)
 {
@@ -40,7 +72,26 @@ static int	ft_hexlen(unsigned int num)
 }
 
 /*
- * Function to print an unsigned integer in hexadecimal format.
+ * Function: ft_print_hex
+ * ----------------------
+ * Prints an unsigned integer in hexadecimal format and returns  
+ * the number of characters printed.  
+ *
+ * Parameters:
+ * - num: The unsigned integer to print in hexadecimal.  
+ * - specifier: Determines if the output should be lowercase ('x') or  
+ *   uppercase ('X').  
+ *
+ * How it works:
+ * 1. Selects the correct `base` string based on the `specifier`.  
+ * 2. Calls `ft_puthex()` to print the number in hexadecimal format.  
+ * 3. Uses `ft_hexlen()` to return the total length of the printed output.  
+ *
+ * Return:
+ * - The number of characters printed.  
+ *
+ * Example:
+ * Calling `ft_print_hex(42, 'x')` prints `"2a"` and returns `2`.  
  */
 int	ft_print_hex(unsigned int num, char specifier)
 {
